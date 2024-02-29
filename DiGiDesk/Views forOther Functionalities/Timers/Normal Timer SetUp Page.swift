60efa6bc1d89ff49b5c79b27ebfb572c6bd3dc56
KOUTAN-App-Builders <gikou21doc@gmail.com>
2024-03-01 00:10:13 +0900

    Made changes in Timers UI and logic for Normal Timer. Changed the UI for Timers Home Page for easier usage. Added logic to normal timer page.

--- DiGiDesk/Views forOther Functionalities/Timers/Normal Timer SetUp Page.swift
+++ DiGiDesk/Views forOther Functionalities/Timers/Normal Timer SetUp Page.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct Normal_Timer_SetUp_Page: View {
+    
+    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    @State var currentDate: Date = Date()
+    // Current Date
+    /*
+    var dateFormatter: DateFormatter{
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        return formatter
+    }
+     */
+    
+    @State var count: Int = 10
+    @State var finishedText: String? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack{
+                Text(finishedText ?? "\(count)")
+            }
+        }
+        .onReceive(timer, perform: { _ in
+            if count <= 1{
+                finishedText = "Done"
+            }else{
+                count -= 1
+            }
+        })
     }
 }
 
--- DiGiDesk/Views forOther Functionalities/Timers/Timers Home Page.swift
+++ DiGiDesk/Views forOther Functionalities/Timers/Timers Home Page.swift
@@ -9,30 +9,76 @@ import SwiftUI
 
 struct Timers_Home_Page: View {
     var body: some View {
-        ScrollView{
-            Section("Timer Type") {
+        VStack(alignment: .leading, spacing: 10){
                 NavigationLink {
                     Normal_Timer_SetUp_Page()
                 } label: {
-                    Text("Ordinary Timer")
+                    HStack{
+                        Image(systemName: "timer")
+                            .padding(.trailing)
+                        VStack(alignment: .leading){
+                            Text("Ordinary Timer")
+                                .font(.headline)
+                                .bold()
+                            Text("Creates an unrepeated ordinary timer.")
+                                .font(.subheadline)
+                        }
+                    }
                 }
+                .padding(.vertical)
+            Divider()
                 NavigationLink {
                     Pomodoro_Timer_SetUp_Page()
                 } label: {
-                    Text("Pomodoro Timer")
+                    HStack{
+                        Image(systemName: "brain.head.profile")
+                            .padding(.trailing)
+                        VStack(alignment: .leading){
+                            Text("Pomodoro Timer")
+                                .font(.headline)
+                                .bold()
+                            Text("Creates a Pomodoro Session.")
+                                .font(.subheadline)
+                        }
+                    }
                 }
+                .padding(.vertical)
+            Divider()
                 NavigationLink {
                     Repetition_Timer_SetUp_Page()
                 } label: {
-                    Text("Infinite Repetition Timer")
+                    HStack{
+                        Image(systemName: "repeat")
+                            .padding(.trailing)
+                        VStack(alignment: .leading){
+                            Text("Infinite Repetition Timer")
+                                .font(.headline)
+                            Text("Creates a timer with repetition.")
+                                .font(.subheadline)
+                        }
+                    }
                 }
+                .padding(.vertical)
+            Divider()
                 NavigationLink {
                     Test_Sim_Timer_SetUp_Page()
                 } label: {
-                    Text("Test Simulation Timer")
+                    HStack{
+                        Image(systemName: "pencil.and.list.clipboard")
+                            .padding(.trailing)
+                        VStack(alignment: .leading){
+                            Text("Test Simulation Timer")
+                                .font(.headline)
+                                .bold()
+                            Text("Creates a timer to simulate real life exams.")
+                                .font(.subheadline)
+                        }
+                    }
                 }
+                .padding(.vertical)
             }
-        }
+        .padding(.leading, 30)
+        .navigationTitle("Timer Type")
     }
 }
 
